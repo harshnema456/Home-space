13d83949f237395d0ef96a7934e194e13eed17ca
HARSH NEMA <harshnema456@gmail.com>
2025-09-27 13:15:47 +0530

    Update gallery tab content and site title
    Revised the titles and descriptions for images in GalleryTab.jsx to provide more detailed and thematic information. Updated the HTML page title from 'Vite + React' to 'ASTRO.IGNIS'. Upgraded Vite dependency from 7.1.2 to 7.1.7 in package.json.

--- src/AllComponents/SpaveNova/GalleryTab.jsx
+++ src/AllComponents/SpaveNova/GalleryTab.jsx
@@ -5,26 +5,27 @@ function GalleryTab() {
     {
       imgSrc: '/image1.jpeg',
       alt: 'Orion Nebula',
-      title: 'Orion Nebula',
-      description: 'A stellar nursery located 1,344 light-years from Earth. One of the most studied objects in the night sky.'
+      title: 'DEATH OF THE STAR',
+      description: 'its an image taken from james web satallite . A stellar nursery located 1,344 light-years from Earth.its shows the death of the star. One of the most studied objects in the night sky.'
     },
     {
       imgSrc: '/image2.jpeg',
       alt: 'Crab Nebula',
-      title: 'Crab Nebula',
-      description: 'The remnant of a supernova observed in 1054 AD. Contains a pulsar spinning 30 times per second.'
+      title: 'BIRTH OF THE STAR ',
+      description: 'Its image of birth snf death of the stars. the crab nebula is a supernova remnant and pulsar wind nebula in the constellation of the taurus.'
     },
+     
     {
       imgSrc: '/image3.jpeg',
       alt: 'Leopard Spot on Mars',
-      title: 'Leopard Spot on Mars',
-      description: 'A feature on Mars being investigated by the Perseverance Rover. The image highlights olivine and leopard spot targets.'
+      title: 'FOSSILE ON MARS',
+      description: 'A feature on Mars being investigated by the Perseverance Rover. its shows the fossile on the mars and proof of the life .The image highlights olivine and leopard spot targets.'
     },
     {
       imgSrc: '/image4.jpeg',
       alt: 'Andromeda Galaxy',
-      title: 'Andromeda Galaxy',
-      description: 'Our nearest major galactic neighbor, containing over 1 trillion stars and approaching the Milky Way.'
+      title: 'Lettuce To Vitamins',
+      description: 'Astronauts Sunita Williams and Butch Wilmore, whose eight-day mission turned into nine months on the International Space Station, are finally heading back to Earth aboard the SpaceX Dragon spacecraft. They have undocked from the ISS and are on their way. '
     },
   ];
 
